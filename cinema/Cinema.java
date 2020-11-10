ddd2ac71ff8dd3946591610150e03c0f10139508
jimenega <jimenega@users.noreply.github.com>
2020-11-09 19:22:14 -0600

    Tickets started

--- cinema/Cinema.java
+++ cinema/Cinema.java
@@ -5,13 +5,17 @@ import java.util.Scanner;
 
 public class Cinema {
 
-    static char [][] cinema = new char [7][8];
+    //static char [][] cinema = new char [7][8];
+    static char [][] cinema;
 
     public static void populateCinema() {
-        for (int i = 0; i < cinema.length; i++) {
-            for ( int j = 0; j < cinema.length + 1; j++)
+        cinema = new char[ScreenRooms.rows][ScreenRooms.seats];
+        for (int i = 0; i < ScreenRooms.rows; i++) {
+            for ( int j = 0; j < ScreenRooms.seats; j++) {
                 cinema[i][j] = 'S';
+            }
         }
+        System.out.println(Arrays.deepToString(cinema));
     }
 
     public static String formatString(int row) {
@@ -26,16 +30,25 @@ public class Cinema {
         int cinemaRow = 0;
         //String columnHeading = "  1 2 3 4 5 6 7 8";
         System.out.println("Cinema:");
-        System.out.println("  1 2 3 4 5 6 7 8"); //column heading
-        for (int rowHeading = 1; rowHeading <= 7; rowHeading++ )
+        //System.out.println("  1 2 3 4 5 6 7 8"); //column heading
+        System.out.printf("%s", "  ");
+        for (int columnHeading = 1; columnHeading <= ScreenRooms.seats; columnHeading++ ) {
+            System.out.printf("%d ", columnHeading);
+            //columnHeading++;
+        }
+        System.out.println();
+        for (int rowHeading = 1; rowHeading <= ScreenRooms.rows; rowHeading++ ) {
             System.out.printf("%d %s%n", rowHeading, formatString(cinemaRow));
+            cinemaRow++;
+        }
     }
 
     public static void main(String[] args) {
-        //populateCinema();
-        //showSeating();
+
         ScreenRooms screenRoom1 = new ScreenRooms();
         screenRoom1.calculateProfit();
+        populateCinema();
+        showSeating();
     }
 }
 
@@ -75,7 +88,7 @@ public class Cinema {
             + (BUDGET_PRICE * budgetRowCountEven * seats);
         }
         System.out.println("Total income:");
-        System.out.printf("$%d",profit);
+        System.out.printf("$%d%n%n",profit);
      }
 
      public void getFloorPlan() {
